61bf97c480d6cacdb4ee48f5e5b222a13276dd79
Ahmad Ghozi Darmawan <ahmadghozi106@gmail.com>
2025-07-09 19:39:45 +0700

    feat<register>: add registration page with form fields and styling

--- nongki-client/src/main.jsx
+++ nongki-client/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
+import Register from './pages/Register';
 import Order from './pages/Order';
 import Map from './pages/Map';
 import Confirm from './pages/Confirm';
@@ -13,6 +14,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Routes>
         <Route path="/" element={<Landing />} /> 
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route path="/order" element={<Order />} />
         <Route path="/map" element={<Map />} />
         <Route path="/confirm" element={<Confirm />} />
